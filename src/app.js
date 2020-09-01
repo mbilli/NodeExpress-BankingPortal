64abbaac1d77dd100d11ea1421f9a80bf21ae4cc
Matias Billi <matias.cruz.billi@accenture.com>
2020-09-01 16:48:28 -0300

    module 2

--- src/app.js
+++ src/app.js
@@ -4,13 +4,34 @@ const express = require('express');
 
 const app = express();
 
-app.set('views', path.join(__dirname,'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const accountData = fs.readFileSync('src/json/accounts.json', { encoding: 'utf8', flag: 'r' });
+const accounts = JSON.parse(accountData);
+const userData = fs.readFileSync('src/json/users.json', { encoding: 'utf8', flag: 'r' });
+const users = JSON.parse(userData);
+
 app.get('/', (req, res) => {
-  res.render('index', {title:'Index'});
+  res.render('index', { title: 'Account Summary', accounts: accounts });
+});
+
+app.get('/savings', (req, res) => {
+  res.render('account', { account: accounts.savings })
+});
+
+app.get('/checking', (req, res) => {
+  res.render('account', { account: accounts.checking })
+});
+
+app.get('/credit', (req, res) => {
+  res.render('account', { account: accounts.credit })
+});
+
+app.get('/profile', (req, res) => {
+  res.render('profile', { user: users[0] })
 });
 
 app.listen(3000, () => console.log('listening at port 3000'));
